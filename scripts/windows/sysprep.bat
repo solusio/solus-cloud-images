6d38cfd4e21314b7e9955a5e3da22dc1089dcea2
Plsk-test <tes.testy@mail.ru>
2020-08-11 09:29:55 +0200

    On branch patch-2  Changes to be committed: 	new file:   viodrv/Balloon/2k19/amd64/balloon.cat 	new file:   viodrv/Balloon/2k19/amd64/balloon.inf 	new file:   viodrv/Balloon/2k19/amd64/balloon.pdb 	new file:   viodrv/Balloon/2k19/amd64/balloon.sys 	new file:   viodrv/certificate.cer 	new file:   viodrv/guest-agent/qemu-ga-x86_64.msi 	new file:   viodrv/vioscsi/2k19/amd64/vioscsi.cat 	new file:   viodrv/vioscsi/2k19/amd64/vioscsi.inf 	new file:   viodrv/vioscsi/2k19/amd64/vioscsi.pdb 	new file:   viodrv/vioscsi/2k19/amd64/vioscsi.sys 	new file:   viodrv/vioserial/2k19/amd64/vioser.cat 	new file:   viodrv/vioserial/2k19/amd64/vioser.inf 	new file:   viodrv/vioserial/2k19/amd64/vioser.pdb 	new file:   viodrv/vioserial/2k19/amd64/vioser.sys 	modified:   scripts/windows/sysprep.bat 	modified:   windows/windows-2019.json

--- scripts/windows/sysprep.bat
+++ scripts/windows/sysprep.bat
@@ -1,6 +1,20 @@
 net stop tiledatamodelsvc
+
+msiexec /i c:\Windows\Temp\viodrv\guest-agent\qemu-ga-x86_64.msi
+sc config qemu-ga start=auto
+sc config "QEMU Guest Agent VSS Provider" start=auto
+certutil -addstore "TrustedPublisher" c:\Windows\Temp\viodrv\certificate.cer
+RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 c:\Windows\Temp\viodrv\vioserial\2k19\amd64\vioser.inf
+RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 c:\Windows\Temp\viodrv\vioscsi\2k19\amd64\vioscsi.inf
+RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 c:\Windows\Temp\viodrv\balloon\2k19\amd64\balloon.inf
+pnputil -i -a c:\Windows\Temp\viodrv\vioserial\2k19\amd64\vioser.inf
+pnputil -i -a c:\Windows\Temp\viodrv\vioscsi\2k19\amd64\vioscsi.inf
+pnputil -i -a c:\Windows\Temp\viodrv\balloon\2k19\amd64\balloon.inf
+@RD /S /Q "c:\Windows\Temp\viodrv"
+
 if exist a:\unattend.xml (
   cmd /c Net user clouduser /active:no
+  copy a:\fixnetwork.ps1 c:\
   c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:a:\unattend.xml
 ) else (
   cmd /c Net user clouduser /active:no
