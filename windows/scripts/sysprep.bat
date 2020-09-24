4b1d086466ae4faf2ac86fe53000578e5d9e8811
root <root@ns3080042.ip-217-182-196.eu>
2020-09-24 04:09:16 +0000

    Changes to be committed: 	modified:   windows/files/cloudbase-init-unattend.conf 	modified:   windows/files/cloudbase-init.conf 	new file:   windows/files/unattend.xml 	modified:   windows/scripts/sysprep.bat 	modified:   windows/windows-2019.json

--- windows/scripts/sysprep.bat
+++ windows/scripts/sysprep.bat
@@ -11,6 +11,9 @@ pnputil -i -a e:\vioserial\2k19\amd64\vioser.inf
 pnputil -i -a e:\vioscsi\2k19\amd64\vioscsi.inf
 pnputil -i -a e:\balloon\2k19\amd64\balloon.inf
 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
+
 if exist a:\unattend.xml (
   cmd /c Net user clouduser /active:no
   c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:a:\unattend.xml
