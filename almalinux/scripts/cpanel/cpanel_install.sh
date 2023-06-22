56180e71e866fc17837a29747180976862c9fa1e
Oleg Neumyvakin <oneumyvakin@plesk.com>
2023-06-22 09:22:04 +0700

    TASK SIO-5239 Add cPanel on AlmaLinux 8. Drop cPanel on CentOS 7.

--- almalinux/scripts/cpanel/cpanel_install.sh
+++ almalinux/scripts/cpanel/cpanel_install.sh
@@ -5,10 +5,6 @@ set -xe
 curl -L "https://securedownloads.cpanel.net/latest" -o /root/setup.sh
 hostnamectl set-hostname my.example.com
 setenforce 0
-systemctl stop NetworkManager
-systemctl disable NetworkManager
-systemctl enable network.service
-systemctl start network.service
 
 sh /root/setup.sh
 /usr/local/cpanel/bin/set_hostname my.example.com
